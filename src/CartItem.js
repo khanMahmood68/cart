3edd014e471f2c94ff5c18db77cc2d4309f1f7eb
khanMahmood68 <mahmoodashrafkhanm0@gmail.com>
2023-09-23 15:50:31 +0530

    Adding Buttons

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,40 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            title:'Phone',
+            price:9999,
+            qty:1,
+            img:''
+        }
+    }
     render(){
+        const {title,price,qty} = this.state
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 9999</div>
-                    <div style={{color:'#777'}}>Qty:1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty:{qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-
-
+                        <img
+                            alt="Increase" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/262/262038.png"/>
+                        <img 
+                            alt="Decrease" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"/>
+                        <img 
+                            alt="Delete" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"/>
                     </div>
                 </div>
             </div>
